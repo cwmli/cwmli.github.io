adfa469f54498b477c1dc187af5826ee35fe03c7
Calvin Li <calvinl9331@gmail.com>
2017-09-24 21:20:28 -0400

    Switched to bootstrap and added avbspeaker project

--- js/cards.js
+++ js/cards.js
@@ -1,57 +1,12 @@
 $(document).ready(function(){
   $(".card").show(1500);
 
-  $("#wdc").mouseover(function(){
-  $("#wdc_c").fadeTo(0, 1);
-  }).mouseleave(function(){
-    $("#wdc_c").fadeTo(0, 0);
-  });
-
-  $("#pen").mouseover(function(){
-    $("#pen_c").fadeTo(0, 1);
-  }).mouseleave(function(){
-    $("#pen_c").fadeTo(0, 0);
-  });
-
-  $("#art").mouseover(function(){
-    $("#art_c").fadeTo(0, 1);
-  }).mouseleave(function(){
-    $("#art_c").fadeTo(0, 0);
-  });
-
-  $("#bl").mouseover(function(){
-    $("#bl_c").fadeTo(0, 1);
-  }).mouseleave(function(){
-    $("#bl_c").fadeTo(0, 0);
-  });
-
-  $("#tt").mouseover(function(){
-    $("#tt_c").fadeTo(0, 1);
-  }).mouseleave(function(){
-    $("#tt_c").fadeTo(0, 0);
-  });
-
-  $("#omb").mouseover(function(){
-    $("#omb_c").fadeTo(0, 1);
-  }).mouseleave(function(){
-    $("#omb_c").fadeTo(0, 0);
-  });
-
-  $("#xd").mouseover(function(){
-    $("#xd_c").fadeTo(0, 1);
-  }).mouseleave(function(){
-    $("#xd_c").fadeTo(0, 0);
-  });
-
-  $("#ogl").mouseover(function(){
-    $("#ogl_c").fadeTo(0, 1);
-  }).mouseleave(function(){
-    $("#ogl_c").fadeTo(0, 0);
-  });
-
-  $("#losh").mouseover(function(){
-    $("#losh_c").fadeTo(0, 1);
-  }).mouseleave(function(){
-    $("#losh_c").fadeTo(0, 0);
+  $("div[class*=card]").mouseover(function(){
+    console.log("yest");
+    $(this).find("div").toggleClass("hide");
+  })
+  
+  $("div[class*=card]").mouseleave(function(){
+    $(this).find("div").toggleClass("hide");
   });
 })
